19e1d138c7625506cafbbffae982d1c6ac742d37
Annette Odingo <odingoannette@gmail.com>
2020-01-16 05:50:56 +0000

    Ensure that server works correctly

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code away!
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res)=>{
+    res.status(200).send('Backend testing');
+
+})
+
+server.listen(5000, ()=>{
+    console.log("listening on port 5000");
+})
+
+
+
